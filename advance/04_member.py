2532176280d9e283afe8ea92198cbf2b091d18c8
Yongjay <yongjay0428@gmail.com>
2025-09-15 14:16:16 +0900

    클래스멤버

--- advance/04_member.py
+++ advance/04_member.py
@@ -1,10 +1,13 @@
 class Robot:
-    #생성자! ->
+    #생성자! -> 객체화 시에 호출되는 함수의 일종으로
+    #객체화가 될 때 가장 먼저 호출된다. 즉, Robot()을 부르면 init으로 호출된다.(복사 역할)
+    #self?
     def __init__(self):
-        pass
+        print("Robot이 복사될때 제일 먼저 호출되는 멤버")
 
     def doit(self):
         print('나: Robot의 함수 입니다.')
 
-robo = Robot()
-robo.doit()
+robot = Robot() #객체화 -> init 호출됨
+robot.doit() # 객체화 된 상태에서 실행하여 호출됨
+
---
+++ advance/05_initial.py
@@ -0,0 +1,7 @@
+class Puppy:
+
+    name=""
+    goal=""
+
+    def __init__(self, name, goal):
+        pass
