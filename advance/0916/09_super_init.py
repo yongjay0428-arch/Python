3a863bd36c0a994d4323b4c20ebd9a5870fc5f57
Yongjay <yongjay0428@gmail.com>
2025-09-16 12:46:30 +0900

    생성자 전달하기

--- advance/0916/09_super_init.py
+++ advance/0916/09_super_init.py
@@ -8,3 +8,18 @@ class Child(Parent):
         print('자식생성자')
 
 ch = Child()
+
+#부모가 초기화가 필요하다면 자식에게 값을 전달해서 자식이 부모에게 전달하도록 한다.
+class SchoolMember(): #클래스만 스네이크 사용 X
+    name = ''
+    age = 0
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Teacher(SchoolMember):
+    salary = 0
+    def __init__(self, name, age, salary): #하나는내껀데 다른건 부모클래스에게 전달
+        super().__init__(name, age)
+        self.salary = salary
