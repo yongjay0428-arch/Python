4d67c043ff8a53e00585aa7411f0953e183c8e37
Yongjay <yongjay0428@gmail.com>
2025-09-16 15:47:34 +0900

    단일,다중 예외처리방식

--- advance/0916/11_handling.py
+++ advance/0916/11_handling.py
@@ -1,11 +1,17 @@
+import traceback
+
 num_list = [1,2,3,1,2,3,1,2,3,1,2,3,4,5,6]
+
 idx = 0
+
 try:
     while True:
         idx = num_list.index(3, idx)
         print(f'3은 {idx}인덱스에 있습니다') #ValueError: 3 is not in list
         idx +=1
-except ValueError:
+except ValueError as e:
+    print(e) #예외에 대한 대략적인 정보 출력
+    traceback.print_exc() #상세한 예외 정보를 보여준다.
     print ('더 이상 3을 찾을 수 없습니다')
 finally:
     print('====끝====')
