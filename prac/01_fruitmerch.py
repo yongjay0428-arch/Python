3a1b1957c7fb3a10c1c9166519c2c07711b9c914
Yongjay <yongjay0428@gmail.com>
2025-09-30 13:49:45 +0900

    단일,다중 예외처리방식

--- prac/01_fruitmerch.py
+++ prac/01_fruitmerch.py
@@ -1,21 +1,10 @@
 def solution(k, m, score):
     score.sort(reverse=True)
-    answer = 0
-    n=0
-    box=len(score) // m
-    for i in range(box):
-            answer += score[m-1]*m
-            for m1 in range(m):
-                score.pop(0)
-                if len(score) < m:
-                    break
-    return answer
-
     answer=0
     box=len(score)//m
     for i in range(1,box+1):
-        answer+= min(score[(i-1)*m:m*i-1])*m
+        answer+= min(score[(i-1)*m:m*i])*m
+        if len(score) < m:
+            break
     return answer
-print(solution(3, 4, [1, 2, 3, 1, 2, 3, 1]))
-print(solution(4, 3, [4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2]))
-
+#https://school.programmers.co.kr/learn/courses/30/lessons/135808
